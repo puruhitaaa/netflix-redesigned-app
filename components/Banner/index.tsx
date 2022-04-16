b7e63c5debab427e328e379fecb60fae6cdcbf6e
puruhitaaa <hughdev101@gmail.com>
2022-04-16 20:07:02 +0700

    [+] Banner fixed and installed textshadow dependency

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,11 +1,49 @@
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { FaPlay } from 'react-icons/fa'
+import { HiInformationCircle } from 'react-icons/hi'
+import { baseUrl } from '../../constants/movie'
+import { Movie } from '../../typings'
 
-const Banner = () => {
+interface Props {
+  netflixOriginals: Movie[]
+}
+
+const Banner = ({ netflixOriginals }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null)
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    )
+  }, [netflixOriginals])
+
   return (
-    <div>
-      {/* <section>
-        <Image />
-      </section> */}
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+      <section className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+          objectFit="cover"
+        />
+      </section>
+
+      <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-sm text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
+
+      <section className="flex items-center space-x-3">
+        <button className="banner-btn bg-white text-black">
+          <FaPlay size={16} className="text-black" />
+          Play
+        </button>
+        <button className="banner-btn bg-[gray]/70">
+          More Info <HiInformationCircle size={20} />
+        </button>
+      </section>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,25 @@ import { requests } from '../utils/requests'
 
 interface Props {
   netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
 }
 
-const Home = ({ netflixOriginals }: Props) => {
+const Home = ({
+  netflixOriginals,
+  trendingNow,
+  topRated,
+  actionMovies,
+  comedyMovies,
+  horrorMovies,
+  romanceMovies,
+  documentaries,
+}: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -18,8 +34,8 @@ const Home = ({ netflixOriginals }: Props) => {
 
       <Header />
 
-      <main>
-        <Banner />
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+        <Banner netflixOriginals={netflixOriginals} />
         <section>
           {/* Row */}
           {/* Row */}
